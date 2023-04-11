ef4a619d14a1c1757d74ff3cbd25f361f15d215b
celesto1 <104173953+celesto1@users.noreply.github.com>
2023-04-11 17:13:20 -0400

    subir

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from flask import Flask, jsonify
-import os
+#pip install flask     /para instala flask
+#entrar en la pagina https://flask.palletsprojects.com/en/2.2.x/quickstart/ y copiar el codigo
+
+from flask import Flask, render_template # se agrega render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def hello_world():
+    return render_template("index.html")  # para que ubique el archivo html, hay q moverlo a la carpeta llamada / templates
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
-
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+# en la consola escribrir flask run
